100b22e4cfc486b7d1339ea5bc4a79cd6ed8dbac
DucPhong08 <phongbye01@gmail.com>
2026-01-25 21:45:55 +0700

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import loader from './config/loader';
 import { dbModules } from './constants';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
