96b4b8f0c2fb5955f240687d092841d999165865
DucPhong08 <phongbye01@gmail.com>
2026-01-31 23:03:47 +0700

    Update

--- src/modules/database/entities/main.entities.ts
+++ src/modules/database/entities/main.entities.ts
@@ -1,5 +1,4 @@
 import { Type } from '@nestjs/common';
 import { UserEntity } from '../../users/entities/user.entity';
-import { ProductEntity } from '../../products/entities/product.entity';
 
-export const MainEntities: Type[] = [UserEntity, ProductEntity];
+export const MainEntities: Type[] = [UserEntity];
